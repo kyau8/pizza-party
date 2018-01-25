8d1c269c5951b87d7c0aac0397e3f3909bbe6f9e
Kitty <kittyyau8@gmail.com>
2018-01-24 22:43:22 -0500

    added page styles

---
+++ dev/scripts/main.js
@@ -0,0 +1,17 @@
+
+const pizza = {};
+
+// A variable to store the name of the topping from the clicked checkbox
+// game.topping;
+
+// A function that stores the name of the topping when the checkbox is clicked
+
+function onClickHandler(e) {
+    console.log(e.value);
+    // const topping = document.
+    // const checkTopping = document.getElementById(topping);
+    // if(checkTopping.checked = true) {
+    //     // Run a function to change the class
+    // }
+}
+
